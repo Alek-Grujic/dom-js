4d71d3fbb8113068176430b0d9667dbcb08caaae
Alek-Grujic <alek.grujic001@outlook.com>
2025-07-31 13:53:08 +0200

    Add guess validation and disable check button on correct guess

--- script.js
+++ script.js
@@ -8,11 +8,38 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
+let startingNumber = Math.floor(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
+  if (!(guess > 0) || !(guess < 21)) {
+    document.querySelector('.message').textContent =
+      'Number must be between 1 and 20!';
+  }
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
+  }
+  // correct number
+  if (guess > 0 && guess < 21) {
+    if (guess === startingNumber) {
+      document.querySelector('.message').textContent = 'Correct number!';
+      document.querySelector('.highscore').textContent = score;
+      document.body.style.background = '#60b347';
+      document.querySelector('.check').disabled = true;
+    }
+    // too high
+    if (guess > startingNumber) {
+      document.querySelector('.message').textContent = 'Too high!';
+      document.querySelector('.score').textContent = --score;
+    }
+    // too low
+    if (guess < startingNumber) {
+      document.querySelector('.message').textContent = 'Too low!';
+      document.querySelector('.score').textContent = --score;
+    }
   }
 });
